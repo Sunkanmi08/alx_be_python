9a1e26669948ed1229f41d687c37ce7dca64b596
sunkanmi <oloyede.sunkanmi18@gmail.com>
2025-09-12 21:23:46 +0100

    fixed the variable issue v2

--- basic_operations.py
+++ basic_operations.py
@@ -6,4 +6,3 @@ print(f"Addition of {number1} and {number2} is {number1+number2}")
 print(f"Subtraction of {number1} and {number2} is {number1-number2}")
 
 print(f"Multiplication of {number1} and {number2} is {number1*number2}")
-
