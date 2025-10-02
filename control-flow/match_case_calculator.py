ab5aef71401574de77e14a9ada82bbea1c97483a
sunkanmi <oloyede.sunkanmi18@gmail.com>
2025-10-02 17:54:04 +0100

    tasko1 v0

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,17 @@
+import sys
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+operator = input("Choose the operation (+, -, *, /):")
+
+match operator:
+    case "+":
+        print(f"The result is {int(num1) + int(num2)}.")
+    case "-":
+        print(f"The result is {int(num1) - int(num2)}.")
+    case "*":
+        print(f"The result is {int(num1) * int(num2)}.")
+    case '/':
+        if num2 == "0":
+            print("Cannot divide by zero.")
+            sys.exit()
+        print(f"The result is {int(num1) / int(num2)}.")
